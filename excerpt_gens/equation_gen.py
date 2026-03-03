cd840a7d1293242eb04eb2879f5ae6c5e4f3c260
Garrett Seibert <garrettseibert24@gmail.com>
2026-03-03 17:47:03 -0600

    final changes

--- excerpt_gens/equation_gen.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-
-fig, ax = plt.subplots(figsize = (4, 1))
-ax.text(0.5, 0.5, r'$\cos(\theta) = \frac{A \cdot B}{||A|| \times ||B||}$', fontsize=20, ha='center', va='center')
-ax.axis('off')
-plt.savefig('output/cosine_similarity_equation.png', dpi = 150, bbox_inches='tight')
